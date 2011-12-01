1585f0e1df08e8854261628f6dbaa8478eb14709
Kimiko Huang <kimikohuang@gmail.com>
2011-12-01 19:36:25 +0800

    NEW - # 174: [PICO] divide distinct classifiers from subprocess4Pico3.py
    https://140.112.137.133/trac/ticket/174

--- DM_PICO/src/readExcel.py
+++ DM_PICO/src/readExcel.py
@@ -14,6 +14,24 @@ import csv, codecs, cStringIO
 import nltk
 import os
 import shutil
+from configobj import ConfigObj
+import logging
+import sys
+
+config = ConfigObj('scirev.cfg')
+
+myLevel = int(config['level'])
+
+logging.basicConfig(level=myLevel)
+
+#LOG_FILENAME = '/home/kimiko/git/DM_PICO/DM_PICO/src/example.log'
+#logging.basicConfig(filename=LOG_FILENAME,level=myLevel)
+#logging.basicConfig(level=myLevel, filename=LOG_FILENAME)
+
+#logging.basicConfig(level='logging.DEBUG')
+
+logging.debug("sys.argv[0]: "+sys.argv[0])
+
 
 #portfolio = csv.reader(open('/home/kimiko/output.csv', "rb"))
 #portfolio = csv.reader(open('intervention.xls', "rb"), dialect='excel')
@@ -34,15 +52,21 @@ def utf_8_encoder(unicode_csv_data):
 
 def fReadExcel():
     dirCwd = os.getcwd()+'/'
-    dirData = 'Data/'
-    dirOutput = 'Output1/'
-    filesInput = ['intervention.csv', 'patient.csv', 'outcome.csv']
+#    dirInputExcel = 'Data/'
+    dirInputExcel = '/home/kimiko/Downloads/'
+    pathOutputBasename = 'Output1/'
+    pathOutputDirname = os.path.expanduser('~')+'/'+pathOutputBasename
+    logging.info('pathOutputDirname: '+ pathOutputDirname)
+#    exit() 
+    
+    ListInputFilename = ['intervention.csv', 'patient.csv', 'outcome.csv']
+#    ListInputFilename = ['intervention.xls', 'patient.xls', 'outcome.xls']
     myStopwords = nltk.corpus.stopwords.words('english')
     ListNormalMethod = ['stpwRemoved', 'wnl', 'porter', 'lancaster']
     
     listMyType = ['stp-', 'wnl-', 'ptr-']
-    
-    #myType = 'stp-'
+
+#    myType = 'stp-'
     myType = 'wnl-'
     #myType = 'ptr-'
     
@@ -54,19 +78,29 @@ def fReadExcel():
         myLancasterStemmer = nltk.LancasterStemmer()
         
     #for myType in listMyType:
+#    pathOutputDirname
+#    if os.path.isdir(dirCwd+pathOutputBasename):
+#        try:
+#    #            shutil.rmtree(LDASubDataDir, ignore_errors, onerror)
+#            shutil.rmtree(dirCwd+pathOutputBasename)
+#        except:
+#            raise
+#    os.mkdir(dirCwd+pathOutputBasename)
     
-    if os.path.isdir(dirCwd+dirOutput):
+    if os.path.isdir(pathOutputDirname):
         try:
     #            shutil.rmtree(LDASubDataDir, ignore_errors, onerror)
-            shutil.rmtree(dirCwd+dirOutput)
+            shutil.rmtree(pathOutputDirname)
         except:
             raise
-    os.mkdir(dirCwd+dirOutput)
+    os.mkdir(pathOutputDirname)
     
-    for fileOne in filesInput:
+    for fileOne in ListInputFilename:
         flagFirsRow = True
-        PubmedFile= dirCwd+dirData+fileOne
-        print 'PubmedFile: ', PubmedFile
+#        PubmedFile= dirCwd+dirInputExcel+fileOne
+        PubmedFile= dirInputExcel+fileOne
+#        print 'PubmedFile: ', PubmedFile
+        logging.info('PubmedFile: '+ PubmedFile)
     #    exit()
         
         iteObj = unicode_csv_reader(open(PubmedFile, "rb"), dialect='excel')
@@ -76,9 +110,13 @@ def fReadExcel():
         
             
     #    with open('wnl-'+PubmedFile[0:-4]+'.txt', 'w') as training:
-        print "dirCwd+dirOutput+myType+fileOne[0:-4]+'.txt': ", dirCwd+dirOutput+myType+fileOne[0:-4]+'.txt'
-    #    exit()
-        with open(dirCwd+dirOutput+myType+fileOne[0:-4]+'.txt', 'w') as training:
+#        print "dirCwd+pathOutputBasename+myType+fileOne[0:-4]+'.txt': ", dirCwd+pathOutputBasename+myType+fileOne[0:-4]+'.txt'
+#        print "pathOutputDirname+myType+fileOne[0:-4]+'.txt': ", pathOutputDirname+myType+fileOne[0:-4]+'.txt'
+        logging.info("pathOutputDirname+myType+fileOne[0:-4]+'.txt': "+ pathOutputDirname+myType+fileOne[0:-4]+'.txt')
+#        exit()
+#        pathOutputDirname
+#        with open(dirCwd+pathOutputBasename+myType+fileOne[0:-4]+'.txt', 'w') as training:
+        with open(pathOutputDirname+myType+fileOne[0:-4]+'.txt', 'w') as training:
             for row in iteObj: # http://love-python.blogspot.com/2008/02/read-csv-file-in-python.html
             #    names.append((data[0], data[1]))
             #    print ', '.join(row)
@@ -102,7 +140,8 @@ def fReadExcel():
                     listContent = [myPorterStemmer.stem(t) for t in listTokensStped]
                 
                 myWriteData = ' '.join(listContent)
-                print 'myWriteData: ', myWriteData
+#                print 'myWriteData: ', myWriteData
+                logging.debug('myWriteData: '+ myWriteData)
     #            print row[2], row[4]
     #            print listTokens
     #            training.write('%d\n' % numberAllyear)
