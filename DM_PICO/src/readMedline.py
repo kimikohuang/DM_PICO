309c767653f2022097382a2ffa823469a8d63a10
Kimiko Huang <kimikohuang@gmail.com>
2011-12-21 18:31:10 +0800

    NEW - # 94: generate no tab pubmed text file. https://140.112.137.133/trac/ticket/94

--- DM_PICO/src/readMedline.py
+++ DM_PICO/src/readMedline.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python
 """
 Filename:
-    readMedline.py
+    DM_PICO/src/readMedline.py
 input
     # Filename: writeConfigObj.py
     scirev.cfg
         config['InputFileLocation'] = "/home/kimiko/Downloads"  
         config['InputFilename'] = "pubmed_result(11).txt"
 output
-    dirCwd+dirOutput+'/'+'pubmed_result_TiAb_org.csv'
-    dirCwd+dirOutput+'/'+'pubmed_result_TiAb_org.txt'
-    dirCwd+dirOutput+'/'+'pubmed_Heading.txt'
-    dirCwd+dirOutput+'/'+'fName_'+strEachKey
+    global_DirMain/Output1/
+    
+    global_DirMain/Output1/strEachKey + '.txt'
+        21922966    investment in innovative mental health care service require 
+        21116752    bone mass , geometry , and tissue material property contrib 
+
+    global_DirMain+InputFilename+'/'+'pubmed_result_TiAb_org.csv'
+        " BACKGROUND: Investment in innovative mental health care services requi
+        " BACKGROUND AND OBJECTIVES: There is no clear gold standard treatment f
+        " BACKGROUND: Bone mass, geometry, and tissue material properties contri
+            or
+        " Therapeutic hypothermia (whole body or selective head cooling) is recognized a
+        " OBJECTIVE: To review and revise the 1987 pediatric brain death guidelines. MET
+        " Therapeutic hypothermia promotes protection after traumatic brain injury (TBI)
+
+    global_DirMain+InputFilename+'/'+'pubmed_result_Ab_Pmid.txt'
+        21922966    background : investment in innovative mental health care ser
+        21922966    economic evaluation is essential to ensure that such an inve
+        21922966    method : a non - systematic review of mental health evaluati
+
+    global_DirMain+InputFilename+'/'+'pubmed_Heading.txt'
+        background : investment in innovative mental health care service require
+        method : a non - systematic review of mental health evaluation identifie
+        result : economic evaluation require the measurement and combination of 
 """
 
 #from lxml import objectify
@@ -40,7 +60,7 @@ logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO)
 
 #logging.basicConfig(level='logging.DEBUG')
 
-logging.debug("sys.argv[0]: "+sys.argv[0])
+logging.info("sys.argv[0]: "+sys.argv[0])
 
 #LEVELS = {'debug': logging.DEBUG,    10
 #          'info': logging.INFO,    20
@@ -52,7 +72,9 @@ logging.debug("sys.argv[0]: "+sys.argv[0])
 
 
 
-global_DirMain =''
+#global_DirMain =''
+global_DirMain = os.path.expanduser('~')+'/' + 'Data/TestDir/' # '/home/kimiko'
+logging.info('global_DirMain = ' + global_DirMain)
 
 
 #def getYearSet(theYear, yearCycle=5, startYear=1941):
@@ -96,9 +118,9 @@ def fReadMedline():
 #    print PubmedFile
     logging.info('PubmedFile: '+ PubmedFile)
 
-    listPICO = ['P','I','O']
+#    listPICO = ['P','I','O']
     ListNormalMethod = ['stpwRemoved', 'wnl', 'porter', 'lancaster']
-    ListHeadings = ['background', '']
+#    ListHeadings = ['background', '']
     myStopwords = nltk.corpus.stopwords.words('english')
 
     #listMyType = ['stp-', 'wnl-', 'ptr-']
@@ -113,22 +135,36 @@ def fReadMedline():
     if 'lancaster' in ListNormalMethod:
         myLancasterStemmer = nltk.LancasterStemmer()
 
-    dirOutput = InputFilename
+#    dirOutput = InputFilename
+#    logging.info('dirOutput = ' + InputFilename)
+    dirOutput = 'Output1'
+    logging.info('dirOutput = ' + dirOutput)
+
     #for typeTextPreprocess in listMyType:
-    dirCwd = os.getcwd()+'/'
-    if os.path.isdir(dirCwd+dirOutput):
+#    global_DirMain = os.getcwd()+'/'
+    logging.info("global_DirMain + dirOutput     = " + global_DirMain+dirOutput)
+    if os.path.isdir(global_DirMain+dirOutput):
         try:
     #            shutil.rmtree(LDASubDataDir, ignore_errors, onerror)
-            shutil.rmtree(dirCwd+dirOutput)
+            shutil.rmtree(global_DirMain+dirOutput)
         except:
             raise
-    os.mkdir(dirCwd+dirOutput)
+    os.mkdir(global_DirMain+dirOutput)
+    
+    logging.info("global_DirMain + InputFilename = " + global_DirMain+InputFilename)
+    if os.path.isdir(global_DirMain+InputFilename):
+        try:
+    #            shutil.rmtree(LDASubDataDir, ignore_errors, onerror)
+            shutil.rmtree(global_DirMain+InputFilename)
+        except:
+            raise
+    os.mkdir(global_DirMain+InputFilename)
     
 
     with open(PubmedFile) as fTxtOrg:
         listDocOrg = fTxtOrg.readlines()
 #    print 'len(listDocOrg): ', len(listDocOrg)
-    logging.debug('len(listDocOrg): ' + str(len(listDocOrg)))
+    logging.info('len(listDocOrg): ' + str(len(listDocOrg)))
     
     
 
@@ -136,27 +172,38 @@ def fReadMedline():
 ##    fNamePubmedTaCsv = global_DirMain+'pubmed_result_TiAb_org.txt'
 ##    fNameHeading = global_DirMain+'fNameHeading.txt'
 ##    fNameHeading = global_DirMain+'fNameHeading.txt'
-    fNameHeading = dirCwd+dirOutput+'/'+'pubmed_Heading.txt'
+#    fNameHeading = global_DirMain+dirOutput+'/'+'pubmed_Heading.txt'
+    fNameHeading = global_DirMain+InputFilename+'/'+'pubmed_Heading.txt'
+    
 #    
     logging.debug('fNameHeading Start: '+ fNameHeading)
 ##    myCsvWriterP = csv.writer(open(fNameHeading, 'wb'), quoting=csv.QUOTE_MINIMAL)
 #
 ##    fNameI = global_DirMain+'fNameI.txt'
-#    fNameI = dirCwd+dirOutput+'/'+'fNameI.txt'
+#    fNameI = global_DirMain+dirOutput+'/'+'fNameI.txt'
 #    
 #    logging.debug('fNameI Start: '+ fNameI)
 ##    myCsvWriterI = csv.writer(open(fNameI, 'wb'), quoting=csv.QUOTE_MINIMAL)
 #
 ##    fNameO = global_DirMain+'fNameO.txt'
-#    fNameO = dirCwd+dirOutput+'/'+'fNameO.txt'
+#    fNameO = global_DirMain+dirOutput+'/'+'fNameO.txt'
 #    
 #    logging.debug('fNameO Start: '+ fNameO)
 ##    myCsvWriterO = csv.writer(open(fNameO, 'wb'), quoting=csv.QUOTE_MINIMAL)
 
     myPunktSentenceTokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-    fNamePubmedTaCsv = global_DirMain+'pubmed_result_TiAb_org.csv'
-    fNamePubmedTaTxt = global_DirMain+'pubmed_result_TiAb_org.txt'
+#    fNamePubmedTaCsv = global_DirMain + 'pubmed_result_TiAb_org.csv'
+#    logging.info('fNamePubmedTaCsv = ' + global_DirMain +'pubmed_result_TiAb_org.csv')
+    fNamePubmedTaCsv = global_DirMain + InputFilename + '/pubmed_result_TiAb_org.csv'
+    logging.info('fNamePubmedTaCsv = ' + fNamePubmedTaCsv)
+#    fNamePubmedTaCsv = global_DirMain + InputFilename + '/pubmed_result_Ab_wPmid.csv'
+#    logging.info('fNamePubmedTaCsv = ' + fNamePubmedTaCsv)
+    
+#    fNamePubmedTaPmidTxt = global_DirMain+'pubmed_result_TiAb_org.txt'
+#    logging.info('fNamePubmedTaPmidTxt = ' + global_DirMain+'pubmed_result_TiAb_org.txt')
+    fNamePubmedTaPmidTxt = global_DirMain + InputFilename + '/pubmed_result_Ab_Pmid.txt'
+    logging.info('fNamePubmedTaPmidTxt = ' + fNamePubmedTaPmidTxt)
 
     flagTi = False
     myTI = ''
@@ -166,14 +213,16 @@ def fReadMedline():
     flagHasAb = False
     strYear = ''
     strOneRecord = ''
-    myCsvWriter = csv.writer(open(dirCwd+dirOutput+'/'+fNamePubmedTaCsv, 'wb'), quoting=csv.QUOTE_MINIMAL)
+#    myCsvWriter = csv.writer(open(global_DirMain+dirOutput+'/'+fNamePubmedTaCsv, 'wb'), quoting=csv.QUOTE_MINIMAL)
+    myCsvWriter = csv.writer(open(fNamePubmedTaCsv, 'wb'), quoting=csv.QUOTE_MINIMAL)
     dicHeading = {}
 
     
-    myFileNamePubmedTaTxt = open(dirCwd+dirOutput+'/'+fNamePubmedTaTxt,"w")
+#    myFileNamePubmedTaPmidTxt = open(global_DirMain+dirOutput+'/'+fNamePubmedTaPmidTxt,"w")
+    myFileNamePubmedTaPmidTxt = open(fNamePubmedTaPmidTxt,"w")
 
     with open(fNameHeading, 'w') as myCsvWriterP:
-    
+        logging.info("len(listDocOrg) = " + str(len(listDocOrg)))
         for oneRow in listDocOrg:
             
                         
@@ -240,7 +289,7 @@ def fReadMedline():
                 if flagSentenceSplitter:
                     listAbsSentence = myPunktSentenceTokenizer.tokenize(strAbstract)
 #                    for rowOflistAbsSentence in listAbsSentence:
-                    
+                logging.debug("len(listTokenOfAbsSentence) = " + str(len(listTokenOfAbsSentence)))
                 for idxOfListTokenOfAbsSentence in range(0,len(listTokenOfAbsSentence)):
                 
                     listContent = listTokenOfAbsSentence[idxOfListTokenOfAbsSentence] # copy problem
@@ -254,10 +303,11 @@ def fReadMedline():
                     if 'wnl-' in  listMyType:
                         listContent = [wnl.lemmatize(t) for t in listContent]
                         logging.debug("'wnl-' in  listMyType: "+' '.join(listContent))
-#                        myFileNamePubmedTaTxt.write(listWriteData.encode("utf-8")+'\n')
-#                        myFileNamePubmedTaTxt.write(listContent.encode("utf-8")+'\n')
-#                        myFileNamePubmedTaTxt.write(' '.join(listContent).encode("utf-8")+'\n')
-                        myFileNamePubmedTaTxt.write(pmid+'\t'+' '.join(listContent).encode("utf-8")+'\n')
+#                        myFileNamePubmedTaPmidTxt.write(listWriteData.encode("utf-8")+'\n')
+#                        myFileNamePubmedTaPmidTxt.write(listContent.encode("utf-8")+'\n')
+#                        myFileNamePubmedTaPmidTxt.write(' '.join(listContent).encode("utf-8")+'\n')
+#                        myFileNamePubmedTaPmidTxt.write(pmid+'\t'+' '.join(listContent).encode("utf-8")+'\n')
+                        myFileNamePubmedTaPmidTxt.write(pmid+' '+' '.join(listContent).encode("utf-8")+'\n')
                         
                     if len(listContent) < 5 or listContent[1]<>':':
                         continue
@@ -272,10 +322,17 @@ def fReadMedline():
 
 #                        print 'listContent: ', listContent
                     
+#                    if listContent[0] not in dicHeading:
+#                        dicHeading[listContent[0]] = [' '.join(listContent[2:-1])]
+#                    else:
+#                        dicHeading[listContent[0]].append(' '.join(listContent[2:-1]))
+                        
                     if listContent[0] not in dicHeading:
-                        dicHeading[listContent[0]] = [' '.join(listContent[2:-1])]
+                        dicHeading[listContent[0]] = [pmid + " " + ' '.join(listContent[2:-1])]
                     else:
-                        dicHeading[listContent[0]].append(' '.join(listContent[2:-1]))
+                        dicHeading[listContent[0]].append(pmid + " " + ' '.join(listContent[2:-1]))
+#                    print dicHeading[listContent[0]]
+                    
                         
 #                    print 'dicHeading[listContent[0]]: ', dicHeading[listContent[0]]
 #                    logging.debug("'wnl-' in  listMyType: "+' '.join(listContent))
@@ -298,17 +355,22 @@ def fReadMedline():
 #                    myCsvWriterP.write(pmid +'_'+ StrIdxOfListTokenOfAbsSentence +' '+ listWriteData.encode("utf-8")+'\n')
                     myCsvWriterP.write(listWriteData.encode("utf-8")+'\n')
                     
+                    
                 
                                     
             strOneRecord = ''
-            
+        # End: for oneRow in listDocOrg:
+        
+        print "len(dicHeading) = ", len(dicHeading)
         for strEachKey in dicHeading.keys():    
-            fNameEachKey = 'fName_'+strEachKey
+#            print "\t".join(dicHeading[strEachKey])
+#            fNameEachKey = 'fName_'+strEachKey
+            fNameEachKey = strEachKey
 #            print 'strEachKey: ', strEachKey, ', len(dicHeading[strEachKey]): ', len(dicHeading[strEachKey])
 #            exit()
             # Create a file object:
             # in "write" mode
-            FILE = open(dirCwd+dirOutput+'/'+fNameEachKey,"w")
+            FILE = open(global_DirMain+dirOutput+'/' + fNameEachKey + '.txt',"w")
             
             # Write all the lines at once:
 #            print len(dicHeading[strEachKey])
@@ -320,6 +382,10 @@ def fReadMedline():
     #            FILE.write(name)
                 
             FILE.close()
+
+#            FilePmid = open(global_DirMain+dirOutput+'/' + fNameEachKey + '-Pmid.txt',"w")
+#            FilePmid.writelines('\n'.join(dicHeading[strEachKey]))
+#            FilePmid.close()
             
 #        for strEachKey in dicHeading.keys():
 #            fNameHeading = 'fName_'+strEachKey
@@ -327,6 +393,6 @@ def fReadMedline():
 ##                myCsvWriterKey.write(listWriteData.encode("utf-8")+'\n')
 #                myCsvWriterKey.write(dicHeading(strEachKey).encode("utf-8")+'\n')
 
-    myFileNamePubmedTaTxt.close()    
+    myFileNamePubmedTaPmidTxt.close()    
 if __name__ == "__main__":
     fReadMedline()
